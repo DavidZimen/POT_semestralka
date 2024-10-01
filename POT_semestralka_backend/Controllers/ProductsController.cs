af84f0b97a9a2f67a314d2540f1a3054b636f741
DavidZimen <david.zimen1@gmail.com>
2024-10-01 22:59:54 +0200

    docker updates

--- POT_semestralka_backend/Controllers/ProductsController.cs
+++ POT_semestralka_backend/Controllers/ProductsController.cs
@@ -20,18 +20,18 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    public ActionResult<IEnumerable<Product>> GetProducts()
     {
         _logger.LogInformation("Received call to GET /products");
-        var products = await _productDbContext.Products.ToListAsync();
+        var products = _productDbContext.Products.ToList();
         return products.Count > 0 ? Ok(products) : NotFound("No products found.");
     }
 
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<Product>> GetProduct(int id)
+    public ActionResult<Product> GetProduct(int id)
     {
         _logger.LogInformation("Received call to GET /products/id");
-        var product = await _productDbContext.Products.FindAsync(id);
+        var product = _productDbContext.Products.Find(id);
         return product != null ? Ok(product) : NotFound($"Product with id {id} not found.");
     }
 
@@ -47,5 +47,24 @@ public class ProductsController : ControllerBase
         await _productDbContext.SaveChangesAsync();
 
         return CreatedAtAction(nameof(GetProduct), new { id = newProduct.ProductId }, newProduct);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        _logger.LogInformation("Received call to DELETE /products/id");
+
+        var productToDelete = await _productDbContext.Products.FindAsync(id);
+        if (productToDelete == null)
+        {
+            var message = $"Product with id {id} not found.";
+            _logger.LogError(message);
+            return NotFound(message);
+        }
+
+        _productDbContext.Products.Remove(productToDelete);
+        await _productDbContext.SaveChangesAsync();
+
+        return NoContent();
     }
 }
--- POT_semestralka_backend/Program.cs
+++ POT_semestralka_backend/Program.cs
@@ -21,6 +21,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    Console.WriteLine("DEVELOPMENT");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
