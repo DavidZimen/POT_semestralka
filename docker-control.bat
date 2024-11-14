8efaece7175ea6f5772fc29bdafb206484d28eca
DavidZimen <david.zimen1@gmail.com>
2024-11-14 15:35:07 +0100

    added bat script to control the docker compose process

---
+++ docker-control.bat
@@ -0,0 +1,53 @@
+@echo off
+setlocal
+
+
+REM Check if an argument is provided
+if "%1"=="" (
+    echo ERROR: Missing argument. Usage: docker_control.bat -up or -down >&2
+    exit /b 1
+)
+
+REM Set the container names to remove after docker-compose down
+set CONTAINER1=api
+set CONTAINER2=client
+
+set IMAGE1=pot_semestralka-pot_frontend
+set IMAGE2=pot_semestralka-pot_api
+
+REM Handle -up argument
+if "%1"=="-up" (
+    echo Starting Docker Compose...
+    docker-compose --env-file .env up --build -d
+    exit /b 0
+)
+
+REM Handle -down argument
+if "%1"=="-down" (
+    echo Stopping Docker Compose...
+    docker-compose down
+
+    REM Remove specified containers
+    echo Removing containers...
+    docker rm -f %CONTAINER1%
+    docker rm -f %CONTAINER2%
+    if errorlevel 1 (
+        echo ERROR: Failed to remove containers. >&2
+        exit /b 1
+    )
+
+    REM Remove specified images
+    echo Removing images...
+    docker rmi -f %IMAGE1%
+    docker rmi -f %IMAGE2%
+    if errorlevel 1 (
+        echo ERROR: Failed to remove images. >&2
+        exit /b 1
+    )
+
+    exit /b 0
+)
+
+REM If invalid argument is provided
+echo ERROR: Invalid argument. Use -up or -down. >&2
+exit /b 1
